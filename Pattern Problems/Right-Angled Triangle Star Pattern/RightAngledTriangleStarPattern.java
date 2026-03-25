a936b56d222b2fa0cd4fb2012a0ff11ced2203d6
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-03-25 08:29:28 +0530

    DSA: Added right-angled triangle star pattern problem

--- Pattern Problems/Right-Angled Triangle Star Pattern/RightAngledTriangleStarPattern.java
+++ Pattern Problems/Right-Angled Triangle Star Pattern/RightAngledTriangleStarPattern.java
@@ -1,3 +1,27 @@
+/*  
+            Problem: Right-Angled Triangle Star Pattern
+  Description:
+        Write a Java program to print a right-angled triangle pattern using stars (*).
+  Input:
+        An integer n representing number of rows.
+  Output: 
+         A right-angled triangle with n rows.
+
+Example:
+Input: 5
+
+Output:
+*
+* *
+* * *
+* * * *
+* * * * *
+Approach:
+
+Prints a triangle using nested loops
+ Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
 class RightAngledTriangleStarPattern {
     public static void main(String[] args) {
         int n = 5; // You can change this value to print a larger or smaller triangle
