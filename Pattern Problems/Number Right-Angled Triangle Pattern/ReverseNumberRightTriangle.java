360691ae2560cd52dbbe20a36793fbee0ed3cc0c
Sheetal Subbannavar <sheetalssubbu@gmail.com>
2026-03-26 18:28:58 +0530

    Update ReverseNumberRightTriangle.java

--- Pattern Problems/Number Right-Angled Triangle Pattern/ReverseNumberRightTriangle.java
+++ Pattern Problems/Number Right-Angled Triangle Pattern/ReverseNumberRightTriangle.java
@@ -21,9 +21,9 @@ Output:
 public class ReverseNumberRightTriangle {
     public static void main(String[] args) {
         int n=5;
-        for(int i=n;i>=0;i--){
-            for(int j=0 ;j<i;j++){
-                System.out.print("*");
+        for(int i=n ;i>0 ;i--){
+            for(int j=1 ;j<=i;j++){
+                System.out.print(j);
             }
             System.out.println();
         }
