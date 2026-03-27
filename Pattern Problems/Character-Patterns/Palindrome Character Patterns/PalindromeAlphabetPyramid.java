0fca4c5c349eaf095e8710e5ce92334d63afd754
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-03-27 12:32:16 +0530

    DSA: Completed palindrome alphabet pyramid pattern

--- Pattern Problems/Character-Patterns/Palindrome Character Patterns/PalindromeAlphabetPyramid.java
+++ Pattern Problems/Character-Patterns/Palindrome Character Patterns/PalindromeAlphabetPyramid.java
@@ -23,22 +23,22 @@ ABCDCBA
 */
 public class PalindromeAlphabetPyramid {
     public static void main(String[] args) {
-        int n=5;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n - i - 1;j++){
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
-            for(int j=0;j<i-1;j++){
-                System.out.print((char)('A'+j));
+            for (int j = 0; j < i - 1; j++) {
+                System.out.print((char) ('A' + j));
             }
-            for(int j=i-1;j>=0;j--){
+            for (int j = i - 1; j >= 0; j--) {
 
-                System.out.print((char)('A'+j));;
+                System.out.print((char) ('A' + j));
+                ;
             }
 
-           
             System.out.println();
-            }
+        }
     }
 }
