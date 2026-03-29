0e28f5436b75997c93a4c19f368dafc2149a0780
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-03-29 10:49:50 +0530

    dsa: valid palindrome using recursion (ignore non-alphanumeric, case-insensitive)

---
+++ Basic Recursion/ValidPalindrome.java
@@ -0,0 +1,45 @@
+/*
+Problem: Valid Palindrome using Recursion
+Write a Java program to check whether a given string is a palindrome.
+Ignore non-alphanumeric characters and consider case-insensitive comparison.
+Input:
+A string s
+Output:
+Return true if the string is a palindrome, else false
+
+Example:
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Explanation:
+After removing non-alphanumeric characters and converting to lowercase:
+"amanaplanacanalpanama" → palindrome
+*/public class ValidPalindrome {
+    public static void main(String[] args) {
+        //String name = "madam";
+        String name = "Anurag";
+         boolean result = isPalindrome(name);
+   
+        System.out.println(result);
+    }
+
+public static boolean isPalindrome(String s) {
+       return check(s, 0, s.length() - 1);   
+    }
+    public static boolean check(String s ,int left,int right){
+         while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {   // for space 
+            left++;
+        }
+        while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            right--;
+        }
+        if (left >= right) {
+            return true;
+        }
+        if (Character.toLowerCase(s.charAt(left)) != 
+            Character.toLowerCase(s.charAt(right))) {
+            return false;
+        }
+       return check(s,left+1,right-1);
+    }
+}
+
