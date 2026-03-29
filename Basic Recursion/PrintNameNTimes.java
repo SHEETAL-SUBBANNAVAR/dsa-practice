7ec0e7c606d2d559791e62d62dc434af7ff055b7
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-03-29 08:59:07 +0530

    Add recursion program to print name N times with base case handling

---
+++ Basic Recursion/PrintNameNTimes.java
@@ -0,0 +1,34 @@
+/*
+Problem: Print Name N Times Using Recursion
+Write a Java program to print a given name N times using recursion.
+Input:
+An integer N (number of times)
+A string (name to print)
+Output:
+Print the given name N times, each on a new line.
+
+Example:
+Input:
+N = 3
+Name = Alex
+Output:
+Alex
+Alex
+Alex
+*/
+public class PrintNameNTimes {
+    public static void main(String[] args) {
+        int n=5;
+        String name="Alex";
+        printNameNTimes(n, name);
+    }
+    public static void printNameNTimes(int n ,String name){
+        if(n==0){
+            return;
+        }else{
+        System.out.println(name);
+        
+        printNameNTimes(n-1,name);
+        }
+    }
+}
