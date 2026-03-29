64ce898262a55a2da014b118ed01f5c1f2ec883c
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-03-29 09:32:25 +0530

    added sum of first n numbers using recursion

--- Basic Recursion/SumOfFirstNNumbers.java
+++ Basic Recursion/SumOfFirstNNumbers.java
@@ -15,14 +15,15 @@ Explanation:
 */
 public class SumOfFirstNNumbers {
     public static void main(String[] args) {
-        int n=5;
+        int n = 5;
         int result = sum(n);
         System.out.println(result);
     }
+
     public static int sum(int n) {
-        
-        if(n==0){
-            return 0;   
+
+        if (n == 0) {
+            return 0;
         }
         return n + sum(n - 1);
 
