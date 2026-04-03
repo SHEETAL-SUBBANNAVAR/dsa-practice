89991a7b1a020eb4ccd289dda56dce3c9bffe479
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-04-03 10:26:07 +0530

    DSA: Implemented Bubble Sort algorithm with optimized loops and detailed dry run explanation

--- sorting-algorithms/Bubble_sort.java
+++ sorting-algorithms/Bubble_sort.java
@@ -48,16 +48,14 @@ public class Bubble_sort {
     }
 
     public static void bubble_sort(int[] arr, int num) {
-        for (int i = 0; i < num-1; i++) {
-            for (int j = 0; j < num-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
+        for (int i = 0; i < num - 1; i++) {
+            for (int j = 0; j < num - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-
             }
-          //System.out.print(arr[i] + " ");
 
         }
         for (int i = 0; i < num; i++) {
