003c5dd3172a594701c2cf2c8f11ee636525f9c8
sheetal subbannavar <sheetalssubbu@gmail.com>
2026-04-24 10:22:46 +0530

    add Kadane's algorithm for maximum subarray sum

---
+++ Array/Medium/MaximumSubarray.java
@@ -0,0 +1,34 @@
+/*        MAXIMUM SUBARRAY (OPTIMAL - KADANE’S ALGORITHM)
+          ===============================================
+Problem: Given an integer array nums[], find the contiguous subarray with the largest sum and return that sum.
+Example: Input: {-2,1,-3,4,-1,2,1,-5,4}          Output: 6   → Subarray: {4,-1,2,1}
+
+Kadane’s Algorithm (Optimal)
+    Logic:- Maintain: currentSum = 0
+                      maxSum = -∞
+          - Traverse array:
+                currentSum += nums[i]
+                Update maxSum:
+                    maxSum = max(maxSum, currentSum)
+                If currentSum < 0:
+                    → Reset currentSum = 0
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+*/
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4 };
+        int result = sum(arr);
+        System.out.println("The max sum is : " +result);
+    }
+    public static int sum(int [] arr){
+        int currentsum=0;
+        int maxSum=arr[0];
+        for(int num:arr){
+            currentsum+=num;
+            maxSum=Math.max(maxSum,currentsum);
+            if(currentsum<0) currentsum=0;
+        }
+        return maxSum;
+    }
+}
